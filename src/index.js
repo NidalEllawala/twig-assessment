733a592e59137da299961c57e314d2c4e476cf97
Arthur Nidal <arthurnidal@gmail.com>
2021-09-21 09:35:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 'use strict'
 //function should return the contents of arr divided into n equally sized arrays
-
 const groupArrayElements = (arr, n) => {
   const result = [];
   const length = arr.length;
@@ -8,27 +7,17 @@ const groupArrayElements = (arr, n) => {
   if (length === 0 ||  length < n) {
     return result;
   //if array can be evenly distributed  
-  } else if (length % n === 0) {
-    distributeEvenly(length, n);
-    return result;
-  //if array can't be evenly distributed and remainder = 1
-  } else if ((length-1)%(n-1) === 0) {
-    distributeEvenly(length-1, n-1);
-    //push remainder to last element of result
-    result.push(arr.slice(-1));
-    return result;
-  //if array can't be evenly distributed and remainder > 1  
-  } else {
-    //get closest possible even distribution
-    let i = length - 1;
-    while ((i)%(n-1) != 0) {
-      i-=1;
-    }
-    distributeEvenly(i, n-1);
-    //push remainder to last element of result
-    result.push(arr.slice(i));
-    return result;
   }
+  //get closest possible even distribution
+  let i = length-1;
+  while ((i)%(n-1) != 0) {
+    i-=1;
+  }
+  distributeEvenly(i, n-1);
+  //push remainder to last element of result
+  result.push(arr.slice(i));
+  return result;
+
   
   //helper function to keep code DRY
   function distributeEvenly(length, n) {
@@ -40,3 +29,5 @@ const groupArrayElements = (arr, n) => {
     }
   }
 }
+
+
